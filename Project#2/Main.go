c86dd9cef9854fb70ba8ccb28e408bcbc0166c04
Leo <leomaz445@gmail.com>
2022-08-07 22:03:59 +0300

    adding the delete method to my list

--- Project#2/Main.go
+++ Project#2/Main.go
@@ -20,32 +20,54 @@ func printMyList(listToPrint *node) {
 		p = p.next
 	}
 }
-func addNode(head *node, newNode node) {
+func addNode(head *node, newNode *node) *node {
 	if head == nil {
-		head = &newNode
-
+		head = newNode
+		return head
 	} else {
 		var p *node = head
 		for head.next != nil {
 			head = head.next
 		}
 
-		head.next = &newNode
+		head.next = newNode
 		head = p
 	}
+	return head
+}
+func deleteByName(head *node, name string) *node {
+	var p *node = head
+	if p.val.name == name {
+		return head.next
+	} else {
+		var temp *node
+		for p.next != nil {
+			temp = p
+			p = p.next
+			if p.val.name == name {
+				temp.next = p.next
+				p.next = nil
+				return head
+			}
+		}
+		temp.next = nil
+	}
+	return head
 }
 func main() {
 	var head *node
 	var user1 node
 	var user2 node
 	var user3 node
-	head = &user1
 	user1.val = user{name: "Leonid", age: 32}
 	user2.val = user{name: "Shlomi", age: 40}
 	user3.val = user{name: "Michael", age: 55}
-	addNode(head, user1)
-	addNode(head, user2)
-	addNode(head, user3)
+	head = addNode(head, &user1)
+	head = addNode(head, &user2)
+	head = addNode(head, &user3)
+	printMyList(head)
+	fmt.Println("after delete test")
+	head = deleteByName(head, "Shlomi")
 	printMyList(head)
 	fmt.Printf("%v", head)
 
